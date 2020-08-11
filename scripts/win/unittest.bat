2df33ba2c6820ef7a55eb9aa7c3b0e7c37522ef9
Pavel Golovatenko-Abramov <p.golovatenko@gmail.com>
2020-08-11 11:49:47 -0400

    added placeholders and scripts

---
+++ scripts/win/unittest.bat
@@ -0,0 +1,12 @@
+@echo off
+set RUNDIR=%cd%
+set ROOT=%~dp0..\..
+set ENV=.env.37
+set TEST=%ROOT%\test
+set FILES=ut_model.py ut_utility.py performance.py
+cd %ROOT%
+(for %%f in (%FILES%) do (
+    echo Running %%f
+    call %ROOT%\%ENV%\Scripts\python.exe %TEST%\%%f -b
+))
+cd %RUNDIR%
