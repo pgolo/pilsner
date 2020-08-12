c142c655ad4007751e2ff04618ac079b6fe04407
Pavel Golovatenko-Abramov <p.golovatenko@gmail.com>
2020-08-12 18:40:42 -0400

    placeholders for unit tests

--- test/ut_model.py
+++ test/ut_model.py
@@ -10,5 +10,41 @@ class TestModel(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_init(self):
+        pass
+
+    def test_del(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_load(self):
+        pass
+
+    def test_add_normalizer(self):
+        pass
+
+    def test_create_recognizer_schema(self):
+        pass
+
+    def test_pack_subtrie(self):
+        pass
+
+    def test_pack_trie(self):
+        pass
+
+    def test_attribute_wrapper(self):
+        pass
+
+    def test_get_dictionary_line(self):
+        pass
+
+    def test_get_dictionary_synonym(self):
+        pass
+
+    def test_next_trie(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
--- test/ut_utility.py
+++ test/ut_utility.py
@@ -10,5 +10,50 @@ class TestModel(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_init(self):
+        pass
+
+    def test_del(self):
+        pass
+
+    def test_push_message(self):
+        pass
+
+    def test_compile_dict_specs(self):
+        pass
+
+    def test_make_recognizer(self):
+        pass
+
+    def test_make_keywords(self):
+        pass
+
+    def test_compile_model(self):
+        pass
+
+    def test_verify_keywords(self):
+        pass
+
+    def test_unpack_trie(self):
+        pass
+
+    def test_check_attrs(self):
+        pass
+
+    def test_attribute_unpacker(self):
+        pass
+
+    def test_spot_entities(self):
+        pass
+
+    def test_flatten_spans(self):
+        pass
+
+    def test_reduce_spans(self):
+        pass
+
+    def test_parse(self):
+        pass
+
 if __name__ == '__main__':
     unittest.main()
