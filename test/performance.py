9bed17a1a86426034f0ffbf8845f2758dd8369fe
Pavel Golovatenko-Abramov <p.golovatenko@gmail.com>
2020-08-22 01:54:54 -0400

    generate content for performance testing

---
+++ test/performance.py
@@ -0,0 +1,77 @@
+import sys; sys.path.insert(0, '')
+import random
+import string
+import timeit
+import pilsner # pylint: disable=E0611,F0401
+
+def random_label(min_words, max_words, min_word_length, max_word_length):
+    number_of_words = random.randint(min_words, max_words)
+    words = []
+    greek = ['qalphaz', 'qbetaz', 'qgammaz']
+    for _ in range(number_of_words):
+        words.append(''.join(random.choice(string.ascii_letters) for i in range(random.randint(min_word_length, max_word_length))))
+    words[0] += greek[random.randint(0, 2)]
+    label = ' '.join(words)
+    return label
+
+def random_label_list(min_labels, max_labels, min_words, max_words, min_word_length, max_word_length):
+    number_of_labels = random.randint(min_labels, max_labels)
+    labels = [random_label(min_words, max_words, min_word_length, max_word_length) for _ in range(number_of_labels)]
+    label_attrs = [','.join([random_label(1, 1, 3, 5) for _ in range(random.randint(1,3))]) for _ in range(number_of_labels)]
+    return labels, label_attrs
+
+def create_labels(dictionary_size, min_labels_per_entity, max_labels_per_entity, min_words_per_label, max_words_per_label, min_word_length, max_word_length):
+    labels = {}
+    label_attrs = {}
+    for i in range(dictionary_size):
+        labels[i+1], label_attrs[i+1] = random_label_list(min_labels_per_entity, max_labels_per_entity, min_words_per_label, max_words_per_label, min_word_length, max_word_length)
+    return labels, label_attrs
+
+def create_entity_attrs(dictionary_size):
+    attrs = {}
+    for i in range(dictionary_size):
+        attrs[i+1] = random_label(1, 1, 3, 5)
+    return attrs
+
+def create_test_dictionary(dictionary_size, min_labels_per_entity, max_labels_per_entity, min_words_per_label, max_words_per_label, min_word_length, max_word_length):
+    labels, label_attrs = create_labels(dictionary_size, min_labels_per_entity, max_labels_per_entity, min_words_per_label, max_words_per_label, min_word_length, max_word_length)
+    entity_attrs = create_entity_attrs(dictionary_size)
+    with open('.test-dict.txt', mode='w', encoding='utf8') as f:
+        for entity_id in entity_attrs:
+            for i in range(len(labels[entity_id])):
+                f.write('%d\t%s\t%s\t%s\n' % (entity_id, labels[entity_id][i], label_attrs[entity_id][i], entity_attrs[entity_id]))
+    selected_entities = [random.randint(1, dictionary_size) for _ in range(10)]
+    with open('.test-labels.txt', mode='w', encoding='utf8') as f:
+        for entity_id in selected_entities:
+            f.write('%s\n' % (labels[entity_id][random.randint(1, len(labels[entity_id]))-1]))
+
+def create_test_text(number_of_words):
+    labels = []
+    with open('.test-labels.txt', mode='r', encoding='utf8') as f:
+        for line in f:
+            labels.append(line.strip())
+    t = []
+    for _ in range(number_of_words):
+        t.append(random_label(1, 1, 3, 10))
+    for label in labels:
+        t.insert(random.randint(1, len(t))-1, label)
+    text = ' '.join(t)
+    with open('.test-text.txt', mode='w', encoding='utf8') as f:
+        f.write(text)
+
+create_test_dictionary(100000, 1, 5, 1, 4, 3, 10)
+create_test_text(100000)
+
+model = pilsner.Model()
+model.add_normalizer('standard', None)
+recognizer = pilsner.Recognizer()
+fields = [
+    {'name': 'entity_id', 'include': True, 'delimiter': None, 'id_flag': True, 'normalizer_flag': False, 'value_flag': False},
+    {'name': 'label', 'include': True, 'delimiter': None, 'id_flag': False, 'normalizer_flag': False, 'value_flag': True},
+    {'name': 'label_attr', 'include': True, 'delimiter': ',', 'id_flag': False, 'normalizer_flag': False, 'value_flag': False},
+    {'name': 'entity_attr', 'include': True, 'delimiter': None, 'id_flag': False, 'normalizer_flag': False, 'value_flag': False}
+]
+specs = recognizer.compile_dict_specs(fields)
+recognizer.compile_model(model, '.test-dict.txt', specs, ' ', '\t', '\n', include_keywords=True)
+
+#print(model)
