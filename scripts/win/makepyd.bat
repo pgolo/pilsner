3be82a1958ac5774280c11ae2caf4e2d4d3325c6
pgolo <p.golovatenko@gmail.com>
2020-09-12 00:19:45 -0400

    think

---
+++ scripts/win/makepyd.bat
@@ -0,0 +1,14 @@
+@echo off
+set RUNDIR=%cd%
+set ROOT=%~dp0..\..
+set ENV=.env.37
+set SRC=%ROOT%\pilsner
+set DIST=%ROOT%\pyd
+set TEST=%ROOT%\test
+cd %ROOT%
+rmdir /S /Q %ROOT%\build
+rmdir /S /Q %ROOT%\cythonized
+if not exist %DIST%\nul mkdir %DIST%
+call %ROOT%\%ENV%\Scripts\python %TEST%\compile.py build_ext --inplace
+move /Y %SRC%\*.pyd %DIST%\
+cd %RUNDIR%
---
+++ test/compile.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+src = [
+    'pilsner/model.py'
+]
+
+setup(ext_modules=cythonize(src, compiler_directives={'language_level': '3'}, build_dir='cythonized'))
