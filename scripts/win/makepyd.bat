b7a31190a40e4fedfc03044d8db5aec710ff6220
pgolo <p.golovatenko@gmail.com>
2020-09-19 00:56:54 -0400

    building and testiing scripts

--- scripts/win/makepyd.bat
+++ scripts/win/makepyd.bat
@@ -8,7 +8,10 @@ set TEST=%ROOT%\test
 cd %ROOT%
 rmdir /S /Q %ROOT%\build
 rmdir /S /Q %ROOT%\cythonized
+rmdir /S /Q %DIST%
 if not exist %DIST%\nul mkdir %DIST%
 call %ROOT%\%ENV%\Scripts\python %TEST%\compile.py build_ext --inplace
 move /Y %SRC%\*.pyd %DIST%\
+copy /Y %SRC%\__init__.py %DIST%\
+copy /Y %SRC%\*.xml %DIST%\
 cd %RUNDIR%
--- test/compile.py
+++ test/compile.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 src = [
-    'pilsner/model.py'
+    'pilsner/model.py',
+    'pilsner/utility.py'
 ]
 
 setup(ext_modules=cythonize(src, compiler_directives={'language_level': '3'}, build_dir='cythonized'))
