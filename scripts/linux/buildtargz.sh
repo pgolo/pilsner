8966b8ee8db4b54180f3ce6a260db404fd38bc26
pgolo <p.golovatenko@gmail.com>
2020-10-08 21:26:10 -0400

    building scripts and chmod (linux)

--- scripts/linux/buildtargz.sh
+++ scripts/linux/buildtargz.sh
@@ -29,6 +29,8 @@ virtualenv -p $1 ${ENV}
 ${ENV}/bin/python3 ${SHIPPING}/make_setup.py sdist
 ${ENV}/bin/python3 ${ROOT}/setup.py sdist
 
+rm -r ${ENV}
+rm -r ${ROOT}/pilsner.egg-info
 rm ${ROOT}/setup.py
 
 cd ${RUNDIR}
